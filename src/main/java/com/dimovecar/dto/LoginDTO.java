9195b0593aab381f26e79f46647ce40a7b07b624
MRZEI <1059330084@qq.com>
2026-06-28 13:15:36 +0800

    feat: 初始化DI挪车后端项目
    - Spring Boot + MyBatis-Plus
    - MySQL + Redis
    - JWT认证

---
+++ src/main/java/com/dimovecar/dto/LoginDTO.java
@@ -0,0 +1,17 @@
+package com.dimovecar.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class LoginDTO {
+
+    @NotBlank(message = "手机号不能为空")
+    private String phone;
+
+    @NotBlank(message = "验证码不能为空")
+    private String code;
+
+    private String codeFrom;
+}
