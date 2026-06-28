3e2b06718b5312e1b78dda75809f741a721c5006
MRZEI <1059330084@qq.com>
2026-06-28 13:15:25 +0800

    feat: 初始化DI挪车后端项目
    - Spring Boot + MyBatis-Plus
    - MySQL + Redis
    - JWT认证

---
+++ src/main/java/com/dimovecar/common/PageResult.java
@@ -0,0 +1,30 @@
+package com.dimovecar.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class PageResult<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long total;
+    private List<T> records;
+    private Long pageNum;
+    private Long pageSize;
+
+    public PageResult() {}
+
+    public PageResult(Long total, List<T> records, Long pageNum, Long pageSize) {
+        this.total = total;
+        this.records = records;
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+    }
+
+    public static <T> PageResult<T> of(Long total, List<T> records, Long pageNum, Long pageSize) {
+        return new PageResult<>(total, records, pageNum, pageSize);
+    }
+}
