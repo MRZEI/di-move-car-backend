e75b6771c2e81806ddad002e9b75fa398608fc02
MRZEI <1059330084@qq.com>
2026-06-28 13:15:50 +0800

    feat: 初始化DI挪车后端项目
    - Spring Boot + MyBatis-Plus
    - MySQL + Redis
    - JWT认证

---
+++ src/main/java/com/dimovecar/mapper/NotificationProgressMapper.java
@@ -0,0 +1,9 @@
+package com.dimovecar.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dimovecar.entity.NotificationProgress;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NotificationProgressMapper extends BaseMapper<NotificationProgress> {
+}
