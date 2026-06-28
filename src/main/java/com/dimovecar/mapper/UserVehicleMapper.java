6ef383f8e3357f6906ef916ca4a2d36750b44dfb
MRZEI <1059330084@qq.com>
2026-06-28 13:15:48 +0800

    feat: 初始化DI挪车后端项目
    - Spring Boot + MyBatis-Plus
    - MySQL + Redis
    - JWT认证

---
+++ src/main/java/com/dimovecar/mapper/UserVehicleMapper.java
@@ -0,0 +1,9 @@
+package com.dimovecar.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dimovecar.entity.UserVehicle;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserVehicleMapper extends BaseMapper<UserVehicle> {
+}
