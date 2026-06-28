48f5a75359da02e0b2348a43e3c3f2cb9a2ac446
MRZEI <1059330084@qq.com>
2026-06-28 13:15:11 +0800

    feat: 初始化DI挪车后端项目
    - Spring Boot + MyBatis-Plus
    - MySQL + Redis
    - JWT认证

---
+++ src/main/java/com/dimovecar/exception/BusinessException.java
@@ -0,0 +1,27 @@
+package com.dimovecar.exception;
+
+import com.dimovecar.common.ResultCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final Integer code;
+
+    public BusinessException(String message) {
+        super(message);
+        this.code = ResultCode.FAIL.getCode();
+    }
+
+    public BusinessException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ResultCode resultCode) {
+        super(resultCode.getMessage());
+        this.code = resultCode.getCode();
+    }
+}
